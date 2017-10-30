d5532e7b1c9c0b656d8324d4a3e8807406768e01
Donovan Goodwin <ddg2goodwin@gmail.com>
2017-10-30 15:29:07 -0600

    V0.1 Code commit
    This is the base code for the mainboard(teensy3.2)
    Not Functional Yet

--- ss-01/ss-01.ino
+++ ss-01/ss-01.ino
@@ -1,32 +1,25 @@
-/*
- * interpreter.ino: Simple Arduino command line interpreter.
- *
- * This is intended solely as a template for building richer,
- * application-specific interpreters. Add your specific commands to the
- * exec() function, and whatever you need to setup() and loop().
- *
- * Usage:
- *   Talk to it through the serial port at 9600/8N1. Commands should be
- *   terminated by CR (\r), answers are terminated by CRLF (\r\n). This
- *   should play well with typical terminal emulators. For the list of
- *   supported commands, see the answer to the "help" command.
- *
- * Copyright (c) 2016 Edgar Bonet Orozco.
- * Released under the terms of the MIT license:
- * https://opensource.org/licenses/MIT
- */
+/*  Project Shade Sort V0.1 Pre-Alpha Code
+//  Developers:
+//    Donovan Goodwin (DGxInfinitY): ddg2goodwin@gmail.com
+//
+//  v0.1:
+//    Basic Code, as of this release the code is not fully functional.
+//
+//
+//
+*/
 
 #include <avr/pgmspace.h>
 #include <Arduino.h>
 #include <EDB.h>
 #include <EEPROM.h>
+#include <PS2Keyboard.h>
 
 #define TABLE_SIZE 512
-#define BUF_LENGTH 128  /* Buffer for the incoming command. */
 
-int shade;
-int size;
-int puckname;
+//
+const int DataPin = 15;
+const int IRQpin =  14;
 
 
 //record info, this is how the record gets recorded.
@@ -49,48 +42,6 @@ byte reader(unsigned long address)  {
 EDB db(&writer, &reader);
 
 
-//Start of the Serial Command reader
-static bool do_echo = true;
-
-
-
-/* Execute a complete command. */
-static void exec(char *cmdline)
-{
-    char *command = strsep(&cmdline, " ");
-
-    if (strcmp_P(command, PSTR("help")) == 0) {
-        Serial.println(F(
-            "addrecord <PuckShade> <Size> <PuckName>: createRecord();\r\n"
-            "read <pin>: digitalRead()\r\n"
-            "aread <pin>: analogRead()\r\n"
-            "write <pin> <value>: digitalWrite()\r\n"
-            "awrite <pin> <value>: analogWrite()\r\n"
-            "echo <value>: set echo off (0) or on (1)"));
-    } else if (strcmp_P(command, PSTR("addrecord")) == 0) {
-
-    } else if (strcmp_P(command, PSTR("read")) == 0) {
-        int pin = atoi(cmdline);
-        Serial.println(digitalRead(pin));
-    } else if (strcmp_P(command, PSTR("aread")) == 0) {
-        int pin = atoi(cmdline);
-        Serial.println(analogRead(pin));
-    } else if (strcmp_P(command, PSTR("write")) == 0) {
-        int pin = atoi(strsep(&cmdline, " "));
-        int value = atoi(cmdline);
-        digitalWrite(pin, value);
-    } else if (strcmp_P(command, PSTR("awrite")) == 0) {
-        int pin = atoi(strsep(&cmdline, " "));
-        int value = atoi(cmdline);
-        analogWrite(pin, value);
-    } else if (strcmp_P(command, PSTR("echo")) == 0) {
-        do_echo = atoi(cmdline);
-    } else {
-        Serial.print(F("Error: Unknown command: "));
-        Serial.println(command);
-    }
-}
-
 void printError(EDB_Status err) {
   Serial.print("ERROR: ");
   switch (err)
@@ -107,9 +58,9 @@ void printError(EDB_Status err) {
       break;
   }
 }
-#define puckname;
-//Create the Records
-void createRecord(int shade, int size) {
+
+//Used to create the new records with the variable, shade, size, and puckname.
+void createRecord(int shade, int size, char* puckname) {
   Serial.printf("Creating Record...");
   newPuck.shade = shade;
   newPuck.size = size;
@@ -119,63 +70,68 @@ void createRecord(int shade, int size) {
   Serial.println("DONE");
 }
 
-void recordLimit()  {
-  Serial.print("Record Limit: ");
-  Serial.println(db.limit());
-}
-
+//Delete one record by specifiing the recordnumber "recno"
 void deleteOneRecord(int recno) {
-  Serial.print("Deleting recno: ");
+  Serial.print("Deleting Record No: ");
   Serial.println(recno);
   db.deleteRec(recno);
 }
-
-void deleteAll()  {
-  Serial.print("Turnacating table...");
-  db.clear();
-  Serial.println("DONE");
-}
-
+//Counts all the records that are in the table.
 void countRecords() {
   Serial.print("Record Count: ");
   Serial.println(db.count());
 }
 
-//Real Stuff Begins
+//Startup Stuff Begins
 void setup()
 {
     Serial.begin(9600);
-    Serial.print("Creating table...");
+    Serial.print("Starting system...");
     // create table at with starting address 0
     db.create(0, TABLE_SIZE, (unsigned int)sizeof(newPuck));
     Serial.println("DONE");
-    recordLimit();
+    //Counts the total ammt. of Records
+    Serial.print("Count Records...");
     countRecords();
-    createRecord(A1, 12);
+    Serial.println("DONE");
+    //Keyboard Load
+    keyboard.begin(DataPin, IRQpin);
+    Serial.print("Keyboard Ready...");
+    Serial.println("DONE")
+    //Create our First Record with the Shade "A1" Size of "!2mm" and the Name of "First"
+    createRecord(A1, 12, First);
 }
 
 void loop() {
+    //Keyboard input search(Right Now it's nonfunctional within the code.)
+    if (keyboard.available()) {
+    // read the next key
+    char c = keyboard.read();
+    // check for some of the special keys
+    if (c == PS2_ENTER) {
+      Serial.println();
+    } else if (c == PS2_TAB) {
+      Serial.print("[Tab]");
+    } else if (c == PS2_ESC) {
+      Serial.print("[ESC]");
+    } else if (c == PS2_PAGEDOWN) {
+      Serial.print("[PgDn]");
+    } else if (c == PS2_PAGEUP) {
+      Serial.print("[PgUp]");
+    } else if (c == PS2_LEFTARROW) {
+      Serial.print("[Left]");
+    } else if (c == PS2_RIGHTARROW) {
+      Serial.print("[Right]");
+    } else if (c == PS2_UPARROW) {
+      Serial.print("[Up]");
+    } else if (c == PS2_DOWNARROW) {
+      Serial.print("[Down]");
+    } else if (c == PS2_DELETE) {
+      Serial.print("[Del]");
+    } else {
 
-    /* Process incoming commands. */
-    while (Serial.available()) {
-        static char buffer[BUF_LENGTH];
-        static int length = 0;
-
-        int data = Serial.read();
-        if (do_echo) Serial.write(data);
-        if ((data == '\b' || data == '\177') && length) {  // BS and DEL
-            length--;
-            if (data == '\177') Serial.write('\b');
-            Serial.write(" \b");
-        }
-        else if (data == '\r') {
-            if (do_echo) Serial.write('\n');    // output CRLF
-            buffer[length] = '\0';
-            if (length) exec(buffer);
-            length = 0;
-        }
-        else if (length < BUF_LENGTH - 1)
-            buffer[length++] = data;
+      // otherwise, just print all normal characters
+      Serial.print(c);
     }
-
+  }
 }
