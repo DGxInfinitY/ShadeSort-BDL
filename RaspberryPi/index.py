6c5af811a97e99e0657d5b036c4301b51e8bd014
DGxInfinitY <ddg2goodwin@gmail.com>
2017-11-15 21:34:44 -0700

    Major Code Shift...
    V0.1 of New Code Tree

---
+++ RaspberryPi/index.py
@@ -0,0 +1,326 @@
+try:
+    # for Python2
+    from Tkinter import *   ## notice capitalized T in Tkinter
+except ImportError:
+    # for Python3
+    from tkinter import *   ## notice lowercase 't' in tkinter here
+
+import tkinter.messagebox as tkMessageBox
+import sqlite3
+import tkinter.ttk as ttk
+
+root = Tk()
+root.title("Shade Sort")
+
+width = 1024
+height = 500
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+x = (screen_width/2) - (width/2)
+y = (screen_height/2) - (height/2)
+root.geometry("%dx%d+%d+%d" % (width, height, x, y))
+root.resizable(0, 0)
+root.config(bg="#99ff99")
+
+#========================================VARIABLES========================================
+USERNAME = StringVar()
+PASSWORD = StringVar()
+PUCK_SHADE = StringVar()
+PUCK_SIZE = IntVar()
+PUCK_NAME = StringVar()
+SEARCH = StringVar()
+
+#========================================METHODS==========================================
+
+def Database():
+    global conn, cursor
+    conn = sqlite3.connect("pythontut.db")
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS `admin` (admin_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT, password TEXT)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS `product` (product_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, puck_shade TEXT, puck_size TEXT, puck_name TEXT)")
+    cursor.execute("SELECT * FROM `admin` WHERE `username` = 'bdl' AND `password` = 'donna1'")
+    if cursor.fetchone() is None:
+        cursor.execute("INSERT INTO `admin` (username, password) VALUES('bdl', 'donna1')")
+        conn.commit()
+
+def Exit():
+    result = tkMessageBox.askquestion('Shade Sort', 'Are you sure you want to exit?', icon="warning")
+    if result == 'yes':
+        root.destroy()
+        exit()
+
+def Exit2():
+    result = tkMessageBox.askquestion('Shade Sort', 'Are you sure you want to exit?', icon="warning")
+    if result == 'yes':
+        Home.destroy()
+        exit()
+
+def ShowLoginForm():
+    global loginform
+    loginform = Toplevel()
+    loginform.title("Shade Sort Login")
+    width = 600
+    height = 500
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    x = (screen_width/2) - (width/2)
+    y = (screen_height/2) - (height/2)
+    loginform.resizable(0, 0)
+    loginform.geometry("%dx%d+%d+%d" % (width, height, x, y))
+    LoginForm()
+
+def LoginForm():
+    global lbl_result
+    TopLoginForm = Frame(loginform, width=600, height=100, bd=1, relief=SOLID)
+    TopLoginForm.pack(side=TOP, pady=20)
+    lbl_text = Label(TopLoginForm, text="Administrator Login", font=('arial', 18), width=600)
+    lbl_text.pack(fill=X)
+    MidLoginForm = Frame(loginform, width=600)
+    MidLoginForm.pack(side=TOP, pady=50)
+    lbl_username = Label(MidLoginForm, text="Username:", font=('arial', 25), bd=18)
+    lbl_username.grid(row=0)
+    lbl_password = Label(MidLoginForm, text="Password:", font=('arial', 25), bd=18)
+    lbl_password.grid(row=1)
+    lbl_result = Label(MidLoginForm, text="", font=('arial', 18))
+    lbl_result.grid(row=3, columnspan=2)
+    username = Entry(MidLoginForm, textvariable=USERNAME, font=('arial', 25), width=15)
+    username.grid(row=0, column=1)
+    password = Entry(MidLoginForm, textvariable=PASSWORD, font=('arial', 25), width=15, show="*")
+    password.grid(row=1, column=1)
+    btn_login = Button(MidLoginForm, text="Login", font=('arial', 18), width=30, command=Login)
+    btn_login.grid(row=2, columnspan=2, pady=20)
+    btn_login.bind('<Return>', Login)
+
+def Home():
+    global Home
+    Home = Tk()
+    Home.title("Shade Sort")
+    width = 1024
+    height = 500
+    screen_width = Home.winfo_screenwidth()
+    screen_height = Home.winfo_screenheight()
+    x = (screen_width/2) - (width/2)
+    y = (screen_height/2) - (height/2)
+    Home.geometry("%dx%d+%d+%d" % (width, height, x, y))
+    Home.resizable(0, 0)
+    Title = Frame(Home, bd=1, relief=SOLID)
+    Title.pack(pady=10)
+    lbl_display = Label(Title, text="Broadway Dental Lab", font=('arial', 45))
+    lbl_display.pack()
+    btn_viewdb = Button(Home, text='View Database', width=25, command=ShowView)
+    btn_viewdb.pack(pady=5)
+    btn_logout = Button(Home, text='Logout', width=25, command=Logout)
+    btn_logout.pack(pady=5)
+    btn_exit = Button(Home, text='Exit', width =25, command=Exit)
+    btn_exit.pack(pady=5)
+    menubar = Menu(Home)
+    filemenu = Menu(menubar, tearoff=0)
+    filemenu2 = Menu(menubar, tearoff=0)
+    filemenu.add_command(label="Logout", command=Logout)
+    filemenu.add_command(label="Exit", command=Exit2)
+    filemenu2.add_command(label="Add new", command=ShowAddNew)
+    filemenu2.add_command(label="View", command=ShowView)
+    menubar.add_cascade(label="Account", menu=filemenu)
+    menubar.add_cascade(label="Inventory", menu=filemenu2)
+    Home.config(menu=menubar)
+    Home.config(bg="#99ff99")
+
+def ShowAddNew():
+    global addnewform
+    addnewform = Toplevel()
+    addnewform.title("Shade Sort/Add new")
+    width = 600
+    height = 500
+    screen_width = Home.winfo_screenwidth()
+    screen_height = Home.winfo_screenheight()
+    x = (screen_width/2) - (width/2)
+    y = (screen_height/2) - (height/2)
+    addnewform.geometry("%dx%d+%d+%d" % (width, height, x, y))
+    addnewform.resizable(0, 0)
+    AddNewForm()
+
+def AddNewForm():
+    TopAddNew = Frame(addnewform, width=600, height=100, bd=1, relief=SOLID)
+    TopAddNew.pack(side=TOP, pady=20)
+    lbl_text = Label(TopAddNew, text="Add New Product", font=('arial', 18), width=600)
+    lbl_text.pack(fill=X)
+    MidAddNew = Frame(addnewform, width=600)
+    MidAddNew.pack(side=TOP, pady=50)
+    lbl_puckshade = Label(MidAddNew, text="Puck Shade", font=('arial', 25), bd=10)
+    lbl_puckshade.grid(row=0, sticky=W)
+    lbl_pucksize = Label(MidAddNew, text="Puck Size", font=('arial', 25), bd=10)
+    lbl_pucksize.grid(row=1, sticky=W)
+    lbl_puckname = Label(MidAddNew, text="Puck Name", font=('arial', 25), bd=10)
+    lbl_puckname.grid(row=2, sticky=W)
+    puckshade = Entry(MidAddNew, textvariable=PUCK_SHADE, font=('arial', 25), width=15)
+    puckshade.grid(row=0, column=1)
+    pucksize = Entry(MidAddNew, textvariable=PUCK_SIZE, font=('arial', 25), width=15)
+    pucksize.grid(row=1, column=1)
+    puckname = Entry(MidAddNew, textvariable=PUCK_NAME, font=('arial', 25), width=15)
+    puckname.grid(row=2, column=1)
+    btn_add = Button(MidAddNew, text="Save", font=('arial', 18), width=30, bg="#009ACD", command=AddNew)
+    btn_add.grid(row=3, columnspan=2, pady=20)
+
+def AddNew():
+    Database()
+    cursor.execute("INSERT INTO `product` (puck_shade, puck_size, puck_name) VALUES(?, ?, ?)", (str(PUCK_SHADE.get()), int(PUCK_SIZE.get()), str(PUCK_NAME.get())))
+    conn.commit()
+    PUCK_SHADE.set("")
+    PUCK_SIZE.set("")
+    PUCK_NAME.set("")
+    cursor.close()
+    conn.close()
+
+def ViewForm():
+    global tree
+    TopViewForm = Frame(viewform, width=600, bd=1, relief=SOLID)
+    TopViewForm.pack(side=TOP, fill=X)
+    LeftViewForm = Frame(viewform, width=600)
+    LeftViewForm.pack(side=LEFT, fill=Y)
+    MidViewForm = Frame(viewform, width=600)
+    MidViewForm.pack(side=RIGHT)
+    lbl_text = Label(TopViewForm, text="View Pucks", font=('arial', 18), width=500)
+    lbl_text.pack(fill=X)
+    lbl_txtsearch = Label(LeftViewForm, text="Search", font=('arial', 15))
+    lbl_txtsearch.pack(side=TOP, anchor=W)
+    search = Entry(LeftViewForm, textvariable=SEARCH, font=('arial', 15), width=10)
+    search.pack(side=TOP,  padx=5, fill=X)
+    btn_search = Button(LeftViewForm, text="Search", width=10, command=Search)
+    btn_search.pack(side=TOP, padx=5, pady=5, fill=X)
+    btn_add = Button(LeftViewForm,  text='Add Puck', width=10, command=ShowAddNew)
+    btn_add.pack(side=TOP, padx=5, pady=5, fill=X)
+    btn_delete = Button(LeftViewForm, text="Delete", width=10, command=Delete)
+    btn_delete.pack(side=TOP, padx=5, pady=5, fill=X)
+    btn_reset = Button(LeftViewForm, text="Reset", width=10, command=Reset)
+    btn_reset.pack(side=TOP, padx=5, pady=5, fill=X)
+    scrollbarx = Scrollbar(MidViewForm, orient=HORIZONTAL)
+    scrollbary = Scrollbar(MidViewForm, orient=VERTICAL)
+    tree = ttk.Treeview(MidViewForm, columns=("ProductID", "Puck Shade", "Puck Size", "Puck Name"), selectmode="extended", height=100, yscrollcommand=scrollbary.set, xscrollcommand=scrollbarx.set)
+    scrollbary.config(command=tree.yview)
+    scrollbary.pack(side=RIGHT, fill=Y)
+    scrollbarx.config(command=tree.xview)
+    scrollbarx.pack(side=BOTTOM, fill=X)
+    tree.heading('ProductID', text="ProductID",anchor=W)
+    tree.heading('Puck Shade', text="Puck Shade",anchor=W)
+    tree.heading('Puck Size', text="Puck Size",anchor=W)
+    tree.heading('Puck Name', text="Puck Name",anchor=W)
+    tree.column('#0', stretch=NO, minwidth=0, width=0)
+    tree.column('#1', stretch=NO, minwidth=0, width=0)
+    tree.column('#2', stretch=NO, minwidth=0, width=200)
+    tree.column('#3', stretch=NO, minwidth=0, width=120)
+    tree.column('#4', stretch=NO, minwidth=0, width=120)
+    tree.pack()
+    DisplayData()
+
+def DisplayData():
+    Database()
+    cursor.execute("SELECT * FROM `product`")
+    fetch = cursor.fetchall()
+    for data in fetch:
+        tree.insert('', 'end', values=(data))
+    cursor.close()
+    conn.close()
+
+def Search():
+    if SEARCH.get() != "":
+        tree.delete(*tree.get_children())
+        Database()
+        cursor.execute("SELECT * FROM `product` WHERE `puck_shade` LIKE ?", ('%'+str(SEARCH.get())+'%',))
+        fetch = cursor.fetchall()
+        for data in fetch:
+            tree.insert('', 'end', values=(data))
+        cursor.close()
+        conn.close()
+
+def Reset():
+    tree.delete(*tree.get_children())
+    DisplayData()
+    SEARCH.set("")
+
+def Delete():
+    if not tree.selection():
+       print("ERROR")
+    else:
+        result = tkMessageBox.askquestion('Inventory Warning', 'Are you sure you want to delete this record?', icon="warning")
+        if result == 'yes':
+            curItem = tree.focus()
+            contents =(tree.item(curItem))
+            selecteditem = contents['values']
+            tree.delete(curItem)
+            Database()
+            cursor.execute("DELETE FROM `product` WHERE `product_id` = %d" % selecteditem[0])
+            conn.commit()
+            cursor.close()
+            conn.close()
+
+
+def ShowView():
+    global viewform
+    viewform = Toplevel()
+    viewform.title("Puck Inventory")
+    width = 1024
+    height = 500
+    screen_width = Home.winfo_screenwidth()
+    screen_height = Home.winfo_screenheight()
+    x = (screen_width/2) - (width/2)
+    y = (screen_height/2) - (height/2)
+    viewform.geometry("%dx%d+%d+%d" % (width, height, x, y))
+    viewform.resizable(0, 0)
+    ViewForm()
+
+def Logout():
+    result = tkMessageBox.askquestion('Shade Sort', 'Are you sure you want to logout?', icon="warning")
+    if result == 'yes':
+        admin_id = ""
+        root.deiconify()
+        Home.destroy()
+
+def Login(event=None):
+    global admin_id
+    Database()
+    if USERNAME.get == "" or PASSWORD.get() == "":
+        lbl_result.config(text="Please complete the required field!", fg="red")
+    else:
+        cursor.execute("SELECT * FROM `admin` WHERE `username` = ? AND `password` = ?", (USERNAME.get(), PASSWORD.get()))
+        if cursor.fetchone() is not None:
+            cursor.execute("SELECT * FROM `admin` WHERE `username` = ? AND `password` = ?", (USERNAME.get(), PASSWORD.get()))
+            data = cursor.fetchone()
+            admin_id = data[0]
+            USERNAME.set("")
+            PASSWORD.set("")
+            lbl_result.config(text="")
+            ShowHome()
+        else:
+            lbl_result.config(text="Invalid username or password", fg="red")
+            USERNAME.set("")
+            PASSWORD.set("")
+    cursor.close()
+    conn.close()
+
+def ShowHome():
+    root.withdraw()
+    Home()
+    loginform.destroy()
+
+
+#========================================MENUBAR WIDGETS==================================
+menubar = Menu(root)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="Account", command=ShowLoginForm)
+filemenu.add_command(label="Exit", command=Exit)
+menubar.add_cascade(label="File", menu=filemenu)
+root.config(menu=menubar)
+
+#========================================FRAME============================================
+Title = Frame(root, bd=1, relief=SOLID)
+Title.pack(pady=10)
+
+#========================================LABEL WIDGET=====================================
+lbl_display = Label(Title, text="Shade Sort Puck Manager", font=('arial', 45))
+lbl_display.pack()
+button_display = Button(root, text='Login', width=25, height=2, command=ShowLoginForm)
+button_display.pack()
+
+#========================================INITIALIZATION===================================
+if __name__ == '__main__':
+    root.mainloop()
